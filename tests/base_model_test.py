5564f12aed7a41d3f84528a0102a071205a389b1
ali mansour <1421@holbertonschool.com>
2021-02-21 23:16:41 +0100

    1st push

--- tests/base_model_test.py
+++ tests/base_model_test.py
@@ -4,3 +4,31 @@
 """
 import unittest
 from models.base_model import BaseModel
+
+
+class test_Base_Model(unittest.TestCase):
+
+    def test_save(self):
+        e = BaseModel()
+        s = e.updated_at
+        e.save()
+        self.assertNotEqual(s, e.updated_at)
+
+    def test_to_dict(self):
+        e = BaseModel()
+        x = e.__dict__
+        self.assertIsNotNone(e.to_dict())
+
+    def test_id(self):
+        e = BaseModel()
+        self.assertIsNotNone(self.id)
+
+    def test_created_at(self):
+        e = BaseModel()
+        self.assertIsNotNone(e.created_at)
+
+    def test___str__(self):
+        e = BaseModel()
+        x = ("[{}] ({}) {}".format(e.__class__.__name__,
+                                   e.id, e.__dict__))
+        self.assertEqual(x, e.__str__())
